1d90c5500018256d2208c10944b79f8519508e1b
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2025-08-30 11:13:56 -0300

    ava-02

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+record Rating(byte Stars)
+{
+
+    public static bool operator >(Rating a, Rating b) => a.Stars > b.Stars;
+    public static bool operator <(Rating a, Rating b) => a.Stars < b.Stars;
+
+    public static bool operator >=(Rating a, Rating b) => a.Stars >= b.Stars;
+    public static bool operator <=(Rating a, Rating b) => a.Stars <= b.Stars;
+}
